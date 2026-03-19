1e41e21a309d10b9f479489b9cfbf6d02ae0b9b3
jalgandhi <139359570+jalgandhi@users.noreply.github.com>
2026-03-19 13:18:24 +0530

    Add files via upload

---
+++ JavaScript/Assignments/Assignment 4.js
@@ -0,0 +1,10 @@
+//Assignment 4
+
+let isMember = false;
+let cartValue = 2100;
+
+if ((isMember && cartValue > 1000) || cartValue > 2000) {
+    console.log("WOW!,Free Delivery");
+} else {
+    console.log("Sorry,You are not eligible for free delivery.");
+}
