87c233d4586660e07cac340d21c2afd21167e537
jalgandhi <139359570+jalgandhi@users.noreply.github.com>
2026-03-20 10:19:59 +0530

    Add files via upload

---
+++ JavaScript/Assignments/Assignmnent 1-5.js
@@ -0,0 +1,74 @@
+// JAL GANMDHI_1066
+
+//Assignment 1 
+
+let name = "Suresh";
+let age = 65;
+let purchaseAmount = 6000;
+
+let discount = 0;
+
+if (age < 18) {
+    discount += 10;
+} else if (age >= 60) {
+    discount += 20;
+}
+
+if (purchaseAmount > 5000) {
+    discount += 5;
+}
+
+
+let finalAmount = purchaseAmount - (purchaseAmount * discount / 100);
+
+
+console.log(`Hi ${name}! You received a total discount of ${discount}%. Final amount: ₹${finalAmount}`);
+
+//assignment 2
+
+let correctUsername = "Jal";
+let correctPassword = "1234";
+
+let username = "Jal";
+let password = "1234";
+
+if (username === correctUsername && password === correctPassword) {
+    console.log("Login successful!");
+} else if (username !== correctUsername && password !== correctPassword) {
+    console.log("Both incorrect");
+} else if (username !== correctUsername) {
+    console.log("Incorrect username");
+} else {
+    console.log("Incorrect password");
+}
+
+//Assignment 3
+
+let number = 7;
+let type = "";
+if (number > 0) {
+    type = "positive";
+} else if (number < 0) {
+    type = "negative";
+} else {
+    type = "zero";
+}
+let evenOdd = (number % 2 === 0) ? "even" : "odd";
+
+console.log(`The number ${number} is ${type} and ${evenOdd}.`);
+
+//Assignmenmt 5
+
+let marks = 89;
+
+if (marks < 0 || marks > 100) {
+    console.log("Invalid input");
+} else if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else if (marks >= 50) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
