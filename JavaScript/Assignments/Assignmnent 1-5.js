8187b124335b090cd69258b309b5c5b01361b8d3
jalgandhi <139359570+jalgandhi@users.noreply.github.com>
2026-03-20 10:21:45 +0530

    Update Assignmnent 1-5.js

--- JavaScript/Assignments/Assignmnent 1-5.js
+++ JavaScript/Assignments/Assignmnent 1-5.js
@@ -57,6 +57,17 @@ let evenOdd = (number % 2 === 0) ? "even" : "odd";
 
 console.log(`The number ${number} is ${type} and ${evenOdd}.`);
 
+//Assignment 4
+
+let isMember = false;
+let cartValue = 2100;
+
+if ((isMember && cartValue > 1000) || cartValue > 2000) {
+    console.log("WOW!,Free Delivery");
+} else {
+    console.log("Sorry,You are not eligible for free delivery.");
+}
+
 //Assignmenmt 5
 
 let marks = 89;
